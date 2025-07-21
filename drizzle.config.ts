2cf8f974e2344568cb78fa661d961ca1f3309775
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-07-21 11:08:53 +0530

    use seperate env config for drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,18 @@
-import { type Config } from "drizzle-kit";
+import { createEnv } from "@t3-oss/env-nextjs";
+// TYPES
+import type { Config } from "drizzle-kit";
+import { z } from "zod";
 
-import { env } from "@/env";
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+  },
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
+});
 
 export default {
   schema: "./src/server/db/schema.ts",
