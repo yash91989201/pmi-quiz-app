1e15731e079b7a46dff513633c900521a9b62f79
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-07-21 10:50:56 +0530

    remove usage of next auth on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,35 @@
-import NextAuth from "next-auth";
-// UTILS
-import { authConfig } from "@/config/auth-config";
+import { NextResponse, NextRequest } from "next/server";
 // CONSTANTS
 import { apiAuthPrefix, authRoutes, publicRoutes } from "@/config/routes";
 
-const { auth } = NextAuth(authConfig);
+export function middleware(req: NextRequest) {
+  const nextUrl = req.nextUrl;
+  const sessionToken = req.cookies.get("session-token")?.value;
+  const isLoggedIn = !!sessionToken;
 
-export default auth((req) => {
-  const { nextUrl, auth } = req;
-
-  const isLoggedIn = !!auth;
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  // Decide redirect path based on route
   const notAuthenticatedRedirect = nextUrl.pathname.startsWith("/admin")
     ? "/auth/admin/login"
     : "/auth/login";
 
-  if (isApiAuthRoute) return null;
+  if (isApiAuthRoute) return NextResponse.next();
 
   if (isAuthRoute) {
-    return null;
+    return NextResponse.next();
   }
 
   if (!isLoggedIn && !isPublicRoute) {
-    return Response.redirect(req.nextUrl.origin + notAuthenticatedRedirect);
+    return NextResponse.redirect(
+      new URL(notAuthenticatedRedirect, req.nextUrl.origin),
+    );
   }
 
-  return null;
-});
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
